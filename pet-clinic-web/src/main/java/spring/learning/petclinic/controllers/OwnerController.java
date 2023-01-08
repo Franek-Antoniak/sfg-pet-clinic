0e6af89ab08752cba0fb29f1b7a17a856164459e
Franek Antoniak <franekant123@gmail.com>
2023-01-08 14:58:16 +0100

    feat: MockMvc tests for OwnerController

--- pet-clinic-web/src/main/java/spring/learning/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/spring/learning/petclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package spring.learning.petclinic.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,11 @@ public class OwnerController {
 	public String listOwners(Model model) {
 		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
+	}
+
+	@RequestMapping("/find")
+	public String findOwners(HttpServletResponse response) {
+		response.setStatus(HttpStatus.NOT_IMPLEMENTED.value());
+		return "notimplemented";
 	}
 }
